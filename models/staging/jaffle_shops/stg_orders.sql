b96f4b388e1f5d0c107c0f066532db076212c936
Ashish Chopra <ashishchopra778@gmail.com>
2021-05-28 10:53:57 +0000

    added changes

---
+++ models/staging/jaffle_shops/stg_orders.sql
@@ -0,0 +1,12 @@
+WITH orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from from {{ source('jaffle_shop', 'orders') }}
+
+)
+SELECT * FROM orders
